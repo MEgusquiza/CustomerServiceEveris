298773c299c2ccfeedc096405c499cf7b5912b83
Manuel <manuelrecsa@gmail.com>
2021-06-28 17:33:04 -0500

    add new change about circuite breaker

--- src/main/java/com/bank/manage/customer/controller/CustomerController.java
+++ src/main/java/com/bank/manage/customer/controller/CustomerController.java
@@ -5,10 +5,7 @@ import com.bank.manage.customer.domain.service.impl.CustomerServiceImpl;
 import com.bank.manage.customer.persistence.entity.Customer;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,50 +17,47 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-   
-	private final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
-	
+  private final Logger Logger = LoggerFactory.getLogger(CustomerController.class);
+
 	
     @Autowired
 	private CustomerServiceImpl  customerServiceImpl;
 	
- //  @CircuitBreaker( name = "getCustomerBreak", fallbackMethod = "fallBack") //,
-    	//	groupKey="time",commandKey="time" ,threadPoolKey= "Thread" )    
-   
-    @TimeLimiter(name = "timelimiterSlow", fallbackMethod = "greetingFallBack")
-   // @Bulkhead(name = "greetingBulkhead", fallbackMethod = "greetingFallBack", type = Bulkhead.Type.THREADPOOL)   
-	@GetMapping("/{id}")  
+    @Bulkhead(name = "customer-service", fallbackMethod = "greetingFallBack",type = Bulkhead.Type.THREADPOOL)
+    @TimeLimiter(name = "customer-service")
+    @GetMapping("/{id}")  
 	public ResponseEntity<Mono<Customer>> getCustomer(@PathVariable("id") String id) {	
-		 LOGGER.debug(" EndPoint get by customer {} ", id);
+    	Logger.debug(" EndPoint get by customer {} ", id);
 		return ResponseEntity.ok(customerServiceImpl.getByIdCustomer(id));
      }
 	
     @GetMapping 
     public Flux<Customer> getAllCustomers() {
-    	 LOGGER.info(" EndPoint get by all customers {} ");
+    	Logger.info(" EndPoint get by all customers {} ");
         return customerServiceImpl.getAllCustomer();
     }
     
 	@PostMapping("/save")
 	public ResponseEntity<Mono<Customer>> createCustomer(@RequestBody Customer customer) {
-		 LOGGER.info(" EndPoit get by all customers {} ");
+		Logger.info(" EndPoit get by all customers {} ");
 		return ResponseEntity.ok(customerServiceImpl.createCustomer(customer) );
 	}
 	
     @PutMapping("/update/{id}")
     public Mono<Customer> updateCustomer(@RequestBody Customer customer, @PathVariable String id) {
-    	LOGGER.debug("Updating User with user-id = {}.", id);
+    	Logger.debug("Updating User with user-id = {}.", id);
         return customerServiceImpl.updateCustomer(customer, id);
     }
     
 	 @PostMapping("/delete/{id}")
     public Mono<Void> deleteCustomer(@PathVariable("id") String id){
-		 LOGGER.debug("End point by delete customers ", id);
+		 Logger.debug("End point by delete customers ", id);
         return customerServiceImpl.deleteByIdCustomer(id);
     }
     
